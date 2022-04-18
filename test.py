6a8f5fd2b70fdab2cac05e8258e92393de2418c5
Muhammed Salim <60375799+salimkt@users.noreply.github.com>
2022-04-18 15:47:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,7 @@ import numpy as np
 from scipy.io.wavfile import read
 from featureextraction import extract_features
 
-from importlib import reload
-import sys
-reload(sys)
+import codecs
 # from speakerfeatures import extract_features
 import warnings
 warnings.filterwarnings("ignore")
@@ -30,7 +28,7 @@ gmm_files = [os.path.join(modelpath, fname) for fname in
              os.listdir(modelpath) if fname.endswith('.gmm')]
 
 # Load the Gaussian gender Models
-models = [cPickle.load(open(fname, 'rb')) for fname in gmm_files]
+models = [cPickle.load(codecs.open(fname, 'rb', encoding='utf-8' )) for fname in gmm_files]
 speakers = [fname.split("/")[-1].split(".gmm")[0] for fname in gmm_files]
 
 error = 0
